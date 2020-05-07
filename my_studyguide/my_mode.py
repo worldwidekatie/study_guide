3e2e51b18a15c50ff6335fa36c89cbd61ee23ccf
worldwidekatie <katie.evankodouglas@gmail.com>
2020-05-07 13:13:55 -0600

    did classes and methods and children

---
+++ my_studyguide/my_mode.py
@@ -0,0 +1,44 @@
+class Game:
+    """
+    A general representation of an 
+    abstract game
+    """
+    fun_level = 5
+
+    def __init__(self, player1='Alice', player2='Bob'):
+        self.rounds = 2
+        self.current_round = 0
+        self.player1 = player1
+        self.player2 = player2
+        self.player1_score = 0
+        self.player2_score = 0
+
+    def print_player(self):
+        """Print the players of the game"""
+        print(self.player1, "is playing", self.player2)
+
+    def add_round(self):
+        self.current_round += 1
+    
+    def player1_score_add(self):
+        self.player1_score += 1
+
+    def player2_score_add(self):
+        self.player2_score += 1
+    
+    def report_score(self):
+        print(self.player1, "has", self.player1_score, "points and ",
+        self.player2, "has ", self.player2_score, "points")
+
+
+class TicTacToe(Games):
+    """
+    This is a child class from the games class
+    """
+
+    def __init__(self, player1='Alice', player2='Bob'):
+        x = self.player1
+        o = self.player2
+
+    def x_and_o(self):
+        print(self.x, "is team X and", self.o, "is team O")
